7913a337c4794082df65ec1e8031b60a69f2764a
0779379920 <abuvaabuava@gmail.com>
2021-05-30 19:16:42 +0600

    Initial commit

---
+++ src/com/geekteek/Main.java
@@ -0,0 +1,71 @@
+package com.geekteek;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("Руслан");
+
+       int temperatu = 26;
+
+       boolean is sunny = thrue;
+
+        int numberOfman = 31;
+
+    if (temperatu > 26 is sunny)
+        System.out.println("можно ити гулять");
+
+    if (temperatu < 0 numberOfman > 19)
+
+        System.out.println("остаёмся дома");
+
+    if (temperatu < 0 || numberOfman > 8)
+
+        System.out.println("идём к тёте");
+
+    }
+    public static void main{
+        System.out.println("Иван");
+
+        int temperatu = 17;
+
+        boolean is sunny = thrue;
+
+        int numberOfyoungster = 14;
+
+        if (temperatu > 26 is sunny)
+        System.out.println("идём к бабушки");
+
+        if (temperatu < 9 numberOfyoungster > 19)
+
+        System.out.println("остаёмся дома");
+
+        if (temperatu < 0 || numberOfyoungster >16)
+
+            System.out.println("идём гулять");
+
+
+
+
+    }
+    public static void main{
+        System.out.println("Иван");
+
+        int temperatu = 11;
+
+        boolean is sunny = thrue;
+
+        int numberOfyoungster = 51;
+
+        if (temperatu > 26 is sunny)
+        System.out.println("лажымсья спать");
+
+        if (temperatu > 10 numberOfyoungster < 52)
+
+        System.out.println("идём гулять");
+
+        if (temperatu < - 42 || numberOfyoungster > 99)
+
+            System.out.println("прыгаем с порашута");
+
+    }
